d74baf2216deb851cdc15bc6f067308f65ad2bc9
Veronika Siluyanova <siluyanovan@gmail.com>
2019-09-25 17:19:59 +0300

    unittesting

---
+++ LR3_UnitTesting/LR3_UnitTesting/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3_UnitTesting
+{
+    public class Triangle
+    {
+        public static bool CheckExisting(double x, double y, double z)
+        {
+            if (x <= 0 || y <= 0 || z <= 0)
+                return false;
+            else
+            {
+                if (x + y > z && x + z > y && y + z > x)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LR3_UnitTesting/UnitTests/UnitTest1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LR3_UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumMoreThanSideLength()
+        {
+            double x = 10, y = 20, z = 25;
+            bool expected = true;
+            
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SumLessThanSideLength()
+        {
+            double x = 2.5, y = 10, z = 7.5;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SumEqualsSideLength()
+        {
+            double x = 100, y = 200, z = 300;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EquilateralTriangle()
+        {
+            double x = 15.6, y = 15.6, z = 15.6;
+            bool expected = true;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsoscelesTriangle()
+        {
+            double x = 40, y = 60, z = 40;
+            bool expected = true;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ZeroLengthSide()
+        {
+            double x = 30, y = 0, z = 12;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThreeZeroLengthSides()
+        {
+            double x = 0, y = 0, z = 0;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneNegativeLengthSide()
+        {
+            double x = -2, y = 14, z = 7.5;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoNegativeLengthSides()
+        {
+            double x = -34, y = 28, z = -32;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThreeNegativeLengthSides()
+        {
+            double x = -2, y = -4, z = -6;
+            bool expected = false;
+
+            bool actual = Triangle.CheckExisting(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+    }
+}
