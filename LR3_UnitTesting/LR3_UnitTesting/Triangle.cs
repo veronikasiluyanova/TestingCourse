dc38a9d523ec84739f422e00e0c93c3010e24aa2
Veronika Siluyanova <siluyanovan@gmail.com>
2019-10-01 13:08:44 +0300

    ch directory

--- LR3_UnitTesting/LR3_UnitTesting/Triangle.cs
+++ LR3_UnitTesting/LR3_UnitTesting/Triangle.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LR3_UnitTesting
 {
     public class Triangle
     {
-        public static bool CheckExisting(double x, double y, double z)
+        public static bool IsTriangleExisting(double x, double y, double z)
         {
-            if (x <= 0 || y <= 0 || z <= 0)
-                return false;
-            else
-            {
-                if (x + y > z && x + z > y && y + z > x)
-                    return true;
-                else
-                    return false;
-            }
+            return x > 0 && y > 0 && z > 0 && x + y > z && x + z > y && y + z > x;            
         }
 
         static void Main(string[] args)
---
+++ LR3_UnitTesting/UnitTests/TriangleExistingUnitTesting.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LR3_UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TriangleExistingUnitTesting
+    {
+        [TestMethod]
+        public void SumMoreThanSideLength()
+        {
+            Assert.IsTrue(Triangle.IsTriangleExisting(10, 20, 25));
+        }
+
+        [TestMethod]
+        public void SumLessThanSideLength()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(2.5, 10, 7.5));
+        }
+
+        [TestMethod]
+        public void SumEqualsSideLength()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(100, 200, 300));
+        }
+
+        [TestMethod]
+        public void EquilateralTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangleExisting(15.6, 15.6, 15.6));
+        }
+
+        [TestMethod]
+        public void IsoscelesTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangleExisting(40, 60, 40));
+        }
+
+        [TestMethod]
+        public void ZeroLengthSide()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(30, 0, 12));
+        }
+
+        [TestMethod]
+        public void ThreeZeroLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void OneNegativeLengthSide()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(-2, 14, 7.5));
+        }
+
+        [TestMethod]
+        public void TwoNegativeLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(-34, 28, -32));
+        }
+
+        [TestMethod]
+        public void ThreeNegativeLengthSides()
+        {
+            Assert.IsFalse(Triangle.IsTriangleExisting(-2, -4, -6));
+        }
+        
+    }
+}
