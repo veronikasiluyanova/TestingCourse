1a44ec99e9241b719e496973e82333c653b83f15
Veronika Siluyanova <siluyanovan@gmail.com>
2019-11-17 15:28:06 +0300

    SeleniumWebDriver

--- SeleniumWebDriver/SeleniumWebDriver/WebDriverTests.cs
+++ SeleniumWebDriver/SeleniumWebDriver/WebDriverTests.cs
@@ -10,7 +10,7 @@ namespace SeleniumWebDriver
     public class WebDriverTests
     {
         private const string EmptyFieldError = "Please fill out this field";
-        private const string SameFieldError = "Please fill out this field";
+        private const string TooManyPassengersError = "A maximum of 9 passengers applies per online booking. To continue you may either make multiple bookings or call Air New Zealand Reservations on 1‑800‑262‑1234 to complete your booking. ";
 
         [TestMethod]
         public void EmptyArrivalPlaceFieldTest()
@@ -23,12 +23,11 @@ namespace SeleniumWebDriver
             departurePlaceField.Clear();
             departurePlaceField.SendKeys("Chicago");
 
-            var leaveDatePicker = driver.FindElement(By.XPath("//*[@id='search-leavedate']/div/span"));
-            leaveDatePicker.Click();
-            var leaveDate = driver.FindElement(By.XPath("//*[@id='calendarpanel-0']/div[2]/table/tbody/tr[4]/td[3]/div"));
-            leaveDate.Click();
-            var returnDate = driver.FindElement(By.XPath("//*[@id='calendarpanel-0']/div[2]/table/tbody/tr[4]/td[7]/div"));
-            returnDate.Click();
+            var oneWayOptionButton = driver.FindElement(By.XPath("//*[@id='field-search-journey-type-oneway']"));
+            oneWayOptionButton.Click();
+
+            var leaveDateFiled = driver.FindElement(By.XPath("//*[@id='leaveDate']"));
+            leaveDateFiled.SendKeys("11/26");
 
             var searchButton = driver.FindElement(By.XPath("//*[@type='submit']"));
             searchButton.Click();
@@ -40,31 +39,43 @@ namespace SeleniumWebDriver
         }
 
         [TestMethod]
-        public void SameDepartureAndArrivalPlacesTest()
+        public void TooManyPassengersTest()
         {
             RemoteWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl(@"https://www.airnewzealand.com/");
 
-            var departurePlaceField = driver.FindElement(By.XPath("//*[@id='depart-from']"));
+            var departurePlaceField = driver.FindElement(By.XPath("//INPUT[@id='depart-from']"));
             departurePlaceField.Click();
             departurePlaceField.Clear();
             departurePlaceField.SendKeys("Chicago");
-            var arrivalPlaceField = driver.FindElement(By.XPath("//*[@id='depart-to']"));
+            var arrivalPlaceField = driver.FindElement(By.XPath("//INPUT[@id='depart-to']"));
             arrivalPlaceField.Click();
-            arrivalPlaceField.SendKeys("Chicago");
+            arrivalPlaceField.SendKeys("Sydney");
 
-            var leaveDatePicker = driver.FindElement(By.XPath("//*[@id='search-leavedate']/div/span"));
-            leaveDatePicker.Click();
-            var leaveDate = driver.FindElement(By.XPath("//*[@id='calendarpanel-0']/div[2]/table/tbody/tr[4]/td[1]/div"));
-            leaveDate.Click();
-            var returnDate = driver.FindElement(By.XPath("//*[@id='calendarpanel-0']/div[2]/table/tbody/tr[4]/td[7]/div"));
-            returnDate.Click();
+            var oneWayOptionButton = driver.FindElement(By.XPath("//*[@id='field-search-journey-type-oneway']"));
+            oneWayOptionButton.Click();
+
+            var leaveDateFiled = driver.FindElement(By.XPath("//*[@id='leaveDate']"));
+            leaveDateFiled.SendKeys("11/26");
+
+            var addChildrenPassengerButton = driver.FindElement(By.XPath("//BUTTON[@name='add child']"));
+            addChildrenPassengerButton.Click();
+            addChildrenPassengerButton.Click();
+            addChildrenPassengerButton.Click();
+            addChildrenPassengerButton.Click();
+            addChildrenPassengerButton.Click();
+            addChildrenPassengerButton.Click();
+            var addInfantPassengerButton = driver.FindElement(By.XPath("//BUTTON[@name='add infant']"));
+            addInfantPassengerButton.Click();
+            addInfantPassengerButton.Click();
+            addInfantPassengerButton.Click();
+            addInfantPassengerButton.Click();
 
             var searchButton = driver.FindElement(By.XPath("//*[@type='submit']"));
             searchButton.Click();
 
-            string errorMessage = driver.FindElement(By.XPath("//*[@id='toSelectedOption-error']")).GetAttribute("textContent");
-            Assert.AreEqual(SameFieldError, errorMessage);
+            string errorMessage = driver.FindElement(By.XPath("//*[@id='paxCounts-error']")).GetAttribute("textContent");
+            Assert.AreEqual(TooManyPassengersError, errorMessage);
 
             driver.Quit();
         }
