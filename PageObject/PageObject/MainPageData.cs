1023d4d1a17295ba0c85a723a45d07077263bb35
Veronika Siluyanova <siluyanovan@gmail.com>
2019-12-05 16:17:24 +0300

    new PageObject

--- PageObject/PageObject/MainPageData.cs
+++ PageObject/PageObject/MainPageData.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace PageObject
 {
     public class MainPageData
     {
+        private string DeparturePlace { get; }
+        private string ArrivalPlace { get; }
+        private string LeaveDate { get; }
+        private string ReturnDate { get; }
+
         public MainPageData (string departurePlace, string arrivalPlace, string leaveDate, string returnDate)
         {
             DeparturePlace = departurePlace;
             ArrivalPlace = arrivalPlace;
             LeaveDate = leaveDate;
             ReturnDate = returnDate;
-        }
-
-        public string DeparturePlace { get; }
-        public string ArrivalPlace { get; }
-        public string LeaveDate { get; }
-        public string ReturnDate { get; }
+        }        
     }
 }
